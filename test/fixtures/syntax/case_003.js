8470752f06f7898412396a21dcc1a9e189e7428e
fish <zhengxinlin@gmail.com>
2015-11-09 17:45:18 +0800

    support desc before @api

---
+++ test/fixtures/syntax/case_003.js
@@ -0,0 +1,13 @@
+/*!
+ * api-annotation: test/fixtures/syntax/case_001.js
+ * Authors  : fish <zhengxinlin@gmail.com> (https://github.com/fishbar)
+ * Create   : 2015-10-19 01:33:37
+ * CopyRight 2015 (c) Fish And Other Contributors
+ */
+/**
+ * desc xxx
+ * @api {get} /api/${version}/hello
+ */
+exports.hello = function () {
+
+};
